6a4da69b6736c8862ffe2512ef270918f9576c54
Abdelrahman Safwat <abdosafwat12345@gmail.com>
2026-02-06 11:19:48 +0200

    Terraform AWS infrastructure using reusable modules

---
+++ modules/secrets/main.tf
@@ -0,0 +1,15 @@
+resource "aws_secretsmanager_secret" "db" {
+  name = var.name
+}
+
+resource "aws_secretsmanager_secret_version" "db" {
+  secret_id = aws_secretsmanager_secret.db.id
+
+  secret_string = jsonencode({
+    username = var.db_username
+    password = var.db_password
+    database = var.db_name
+    host     = var.db_host
+    port     = var.db_port
+  })
+}
